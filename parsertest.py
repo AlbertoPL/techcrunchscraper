7fe2b2d4e87417a646a4a7b62a706e908d67bd30
Alberto Pareja-Lecaros <apareja@albertopl.com>
2017-02-15 18:21:11 -0800

    Initial commit of working TechCrunch scraper and unit tests.

---
+++ parsertest.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+import unittest
+import tcparser
+
+class TestParserMethods(unittest.TestCase):
+
+        def test_getArticleInfoNoCompany(self):
+                self.assertEqual(tcparser.getArticleInfo('https://techcrunch.com/2017/02/15/elon-musk-posts-then-deletes-tweets-calling-trumps-immigration-ban-not-right/'), ['n/a', 'n/a', 'Elon Musk posts, then deletes, tweets calling Trump’s immigration ban “not right”', 'https://techcrunch.com/2017/02/15/elon-musk-posts-then-deletes-tweets-calling-trumps-immigration-ban-not-right/'])
+
+        def test_getArticleInfoWithCompany(self):
+                self.assertEqual(tcparser.getArticleInfo('https://techcrunch.com/2017/02/15/google-teams-up-with-kaggle-to-host-100000-video-classification-challenge/'), ['Google', 'http://www.google.com/', 'Google teams up with Kaggle to host $100,000 video classification challenge', 'https://techcrunch.com/2017/02/15/google-teams-up-with-kaggle-to-host-100000-video-classification-challenge/'])
+
+        def test_getArticleUrls(self):
+                self.assertTrue(len(tcparser.getArticleUrls("https://techcrunch.com", ['//h2[@class="post-title"]/node()/@href', '//li[@class="plain-item"]/node()/@href'])) > 0)
+
+if __name__ == '__main__':
+        unittest.main()
---
+++ tcparser.py
@@ -0,0 +1,71 @@
+from lxml import html
+import requests
+import csv
+
+def writeToCsv(articleResults):
+		"""Write articleResults to a CSV file.
+		articleResults in an array containing information about an article's title, URL,
+		and if applicable, the subject company's name and website URL.
+    """
+		with open('output.csv', 'wb') as csvfile:
+				writer = csv.writer(csvfile, escapechar='"', quoting=csv.QUOTE_MINIMAL)
+
+				for row in articleResults:
+						writer.writerow([unicode(s).encode("utf-8") for s in row])
+
+def getArticleInfo(articleUrl):
+		"""Gets the article's title, URL, and if applicable, the subject company's name and
+		website URL. Note that this function will only work for www.techcrunch.com as of 2/15/2017.
+		Returns [companyName, website, title, articleUrl]
+    """
+		article = requests.get(articleUrl)
+		articleTree = html.fromstring(article.content)
+		title = articleTree.xpath('//h1/text()')
+		if title:
+				title = title[0].strip()
+		website = "n/a"
+		companyName = "n/a"
+
+		websiteExists = False
+		companyCard = articleTree.xpath('//li[@class="data-card crunchbase-card active"]')
+		if companyCard:
+				companyCard = companyCard[0]
+				for element in companyCard.iter("strong"):
+						if element.text == "Website":
+								span = element.getnext()
+								a = span[0]
+								website = a.text
+								websiteExists = True
+								break
+				
+				if websiteExists:
+						companyName = articleTree.xpath('//a[@class="cb-card-title-link"]/text()')
+						if companyName:
+								companyName = companyName[0].strip()
+		
+		return [companyName, website, title.encode('utf-8'), articleUrl]
+
+def getArticleUrls(websiteUrl, xpathsToArticleLinks):
+		"""Gets all article URLs from the websiteUrl's first page by using the list of XPaths 
+		provided by xpathsToArticleLinks.
+		Returns all of the article URLs found
+    """
+		page = requests.get(websiteUrl)
+		tree = html.fromstring(page.content)
+
+		urls = []
+		for xpath in xpathsToArticleLinks:
+				urls += tree.xpath(xpath)
+		return urls
+
+def main():
+		articleResults = []
+		urls = getArticleUrls("https://techcrunch.com", ['//h2[@class="post-title"]/node()/@href', '//li[@class="plain-item"]/node()/@href'])
+		
+		for url in urls:
+				articleResults.append(getArticleInfo(url))
+
+		writeToCsv(articleResults)
+
+if __name__ == "__main__":
+    main()
